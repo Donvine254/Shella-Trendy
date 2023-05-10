fac5d3280942e4ee1ae76471ba75dbf951044e5c
donvine mugendi <donvinemugendi@gmail.com>
2023-05-10 18:05:20 +0300

    updated stylesheet

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const baseUrl = "https://api.jsonbin.io/v3/b/644896228e4aa6225e910b8f";
+const featuredUrl =
+  "https://my-json-server.typicode.com/Donvine254/Shella-Trendy/db";
 const menu = document.querySelector(".menu");
 const dropdown = document.querySelector(".dropdown");
 const body = document.body;
@@ -31,7 +33,6 @@ addDropDown();
 //change webpage theme from light to dark. I set the attributes to inherit but with time i can create custom themes
 function toggleThemes() {
   const themeIcon = document.getElementById("theme");
-  const paymentImage=document.getElementById("paySVG");
 
   themeIcon.addEventListener("click", function () {
     if (themeIcon.innerHTML === '<i class="fa-solid fa-moon"></i>') {
@@ -155,22 +156,20 @@ function handleSubscription() {
 handleSubscription();
 
 //fetch data from my API
-let shopping = {};
-let shoppingItems;
-let clothes;
-let shoes;
-let bags;
+// let shopping = {};
+// let shoppingItems;
+// let clothes;
+// let shoes;
+// let bags;
+let featuredItems = [];
 
 async function fetchShoppingItems() {
   try {
-    const response = await fetch(baseUrl);
+    const response = await fetch(featuredUrl);
     if (response.ok) {
-      const shopping = await response.json();
-      shoppingItems = structuredClone(shopping.record);
-      clothes = await structuredClone(shoppingItems.clothes);
-      shoes = await structuredClone(shoppingItems.shoes);
-      bags = await structuredClone(shoppingItems.bags);
-      renderItems(bags, shoes, clothes);
+      const products = await response.json();
+      featuredItems = await structuredClone(products.featuredproduct);
+      renderItems(featuredItems);
     } else {
       throw new Error("404, permission denied");
     }
@@ -184,9 +183,8 @@ fetchShoppingItems();
 //structured clone reduces time and space complexity associated with looping but is not fully supported by all browsers, especially on ios and android devices
 const productsSection = document.getElementById("products");
 
-function renderItems(bags, clothes, shoes) {
-  allProducts = [...bags, ...clothes, ...shoes];
-  for (const item of allProducts) {
+function renderItems(items) {
+  for (const item of items) {
     const product = document.createElement("div");
     product.classList.add("product");
     product.innerHTML = `<box-icon class="favorite"color="gold" name='heart'></box-icon>
@@ -241,6 +239,6 @@ function addToWishList() {
         isLiked = false;
         alert(`This item has been removed from your wishlist`);
       }
-    });  
+    });
   });
 }
